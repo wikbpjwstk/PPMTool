59fc604f132587cfed00f60d3f6194afe3dbb9a3
wiktor <you@example.com>
2019-10-13 12:53:38 +0200

    naprawiany projekt

--- ppmtool/ppmtool/src/main/java/com/wikb/ppmtool/web/ProjectController.java
+++ ppmtool/ppmtool/src/main/java/com/wikb/ppmtool/web/ProjectController.java
@@ -25,7 +25,7 @@ public class ProjectController {
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result)
 
 	{
-
+//
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
 
